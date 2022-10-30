2ba298b4e833911642dc9ee211f88c63674652ec
shivang257 <97958012+shivang257@users.noreply.github.com>
2022-10-30 09:31:54 +0530

    Time: 36 ms (80.00%), Space: 13.8 MB (80.00%) - LeetHub

--- 2457-minimum-addition-to-make-integer-beautiful/2457-minimum-addition-to-make-integer-beautiful.py
+++ 2457-minimum-addition-to-make-integer-beautiful/2457-minimum-addition-to-make-integer-beautiful.py
@@ -1,15 +1,14 @@
 class Solution:
-    def makeIntegerBeautiful(self, n: int, target: int) -> int:
-        def c(n,target):
+    def check(self,n,target):
             s=0
             while(n):
                 s+=(n%10)
                 n//=10
             return s<=target
-        s=0
-        x=10
+    def makeIntegerBeautiful(self, n: int, target: int) -> int:
+        s,x=0,10
         while (True):
-            if c(n,target):
+            if self.check(n,target):
                 return s
             else:
                 t=x-(n%x)
