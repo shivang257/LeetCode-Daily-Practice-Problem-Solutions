a894a14cef3c62d4e643919a2a9764acd18580b1
shivang257 <97958012+shivang257@users.noreply.github.com>
2022-11-03 00:48:23 +0530

    Time: 28 ms (96.75%), Space: 14 MB (37.43%) - LeetHub

--- 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
@@ -1,20 +1,17 @@
 class Solution:
     def minMutation(self, start: str, end: str, bank: List[str]) -> int:
-        bank = set(bank)
-        q = deque([(start, 0)])
-        
+        bank=set(bank)
+        q=deque([(start,0)])
         while q:
-            curr_gene, mutations = q.popleft()
-            
-            if curr_gene == end:
-                return mutations
+            curr,mut=q.popleft()
+            if curr==end:
+                return mut
             
             for i in range(8):
                 for ch in "ACGT":
-                    next_gene = curr_gene[:i] + ch + curr_gene[i+1:]
+                    n=curr[:i]+ch+curr[i+1:]
                     
-                    if next_gene in bank:
-                        bank.remove(next_gene)
-                        q.append((next_gene, mutations + 1))    
-        
+                    if n in bank:
+                        bank.remove(n)
+                        q.append((n,mut+1))
         return -1
