505fa6c43adaf74f96fdaea58b8757aa3f24f2d7
shivang257 <97958012+shivang257@users.noreply.github.com>
2022-11-04 16:32:30 +0530

    Time: 908 ms (30.91%), Space: 16.6 MB (43.88%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,19 +1,26 @@
-class Solution:
-    def numIslands(self, grid: List[List[str]]) -> int:
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        if not grid: return 0
+        r, c = len(grid), len(grid[0])
+        visited = [[False for _ in range(c)] for _ in range(r)]
+
+        def dfs(i, j):
+            if i < 0 or i >= r or j < 0 or j >= c or grid[i][j] == '0' or visited[i][j]:
+                return
+            visited[i][j] = True
+            dfs(i + 1, j)
+            dfs(i - 1, j)
+            dfs(i, j + 1)
+            dfs(i, j - 1)
+
         count = 0
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == '1':
-                    print(i,j)
-                    self.dfs(grid,i,j)
-                    count  += 1
-        #print(grid)
+        for i in range(r):
+            for j in range(c):
+                if not visited[i][j] and grid[i][j] == '1':
+                    dfs(i, j)
+                    count += 1
         return count
-    # use a helper function to flip connected '1's to 0
-    def dfs(self,grid,i,j):
-        grid[i][j] = 0
-        for dr,dc in (1,0), (-1,0), (0,-1), (0,1):
-            r = i + dr
-            c = j + dc
-            if 0 <= r < len(grid) and 0 <= c < len(grid[0]) and grid[r][c]=='1':
-                self.dfs(grid,r,c)
