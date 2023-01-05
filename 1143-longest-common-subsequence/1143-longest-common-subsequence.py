903a0a77bc0a14f6b18ab6dda20788b40d1789cc
shivang257 <97958012+shivang257@users.noreply.github.com>
2023-01-05 15:26:38 +0530

    Time: 1339 ms (16.98%), Space: 39.1 MB (26.21%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,18 +1,10 @@
-from functools import lru_cache
-
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        
-        @lru_cache(maxsize=None)
-        def memo_solve(ptr1, ptr2):
-            if ptr1 == len(text1) or ptr2 == len(text2):
-                return 0
-            
-            # Case 1
-            if text1[ptr1] == text2[ptr2]:
-                return 1 + memo_solve(ptr1+1, ptr2+1)
-        
-            # Case 2
-            else:     
-                return max(memo_solve(ptr1+1, ptr2), memo_solve(ptr1,ptr2+1))
-        return memo_solve(0,0)
+        dp=[[0]*((len(text2)+1)) for i in range(len(text1)+1)]
+        for i in range(len(text1)):
+            for j in range(len(text2)):
+                if text1[i]==text2[j]:
+                    dp[i+1][j+1]=1+dp[i][j]
+                else:
+                    dp[i+1][j+1]=max(dp[i+1][j],dp[i][j+1])
+        return dp[-1][-1]
