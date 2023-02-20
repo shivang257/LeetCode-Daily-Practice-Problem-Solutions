d6448972c839fd1e8a1bdc2efe2f0e5c99eaaaff
shivang257 <97958012+shivang257@users.noreply.github.com>
2023-02-20 14:39:22 +0530

    Added solution - LeetHub

---
+++ Common elements - GFG/common-elements.py
@@ -0,0 +1,38 @@
+#User function Template for python3
+import collections
+class Solution:
+    def commonElements (self,A, B, C, n1, n2, n3):
+        A,B,C=list(set(A)),list(set(B)),list(set(C))
+        e=A+B+C
+        s=collections.Counter(e)
+        ans=[]
+        for i in s:
+            if s[i]==3:
+                ans.append(i)
+        ans.sort()
+        return ans
+        
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+t = int (input ())
+for tc in range (t):
+    n1, n2, n3 = list(map(int,input().split()))
+    A = list(map(int,input().split()))
+    B = list(map(int,input().split()))
+    C = list(map(int,input().split()))
+    
+    ob = Solution()
+    
+    res = ob.commonElements (A, B, C, n1, n2, n3)
+    
+    if len (res) == 0:
+        print (-1)
+    else:
+        for i in range (len (res)):
+            print (res[i], end=" ")
+        print ()
+
+# } Driver Code Ends
