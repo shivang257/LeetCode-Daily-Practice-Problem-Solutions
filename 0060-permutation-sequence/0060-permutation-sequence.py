0e037efdb3afa7bc52b5f06d15f8d831e57d24e3
shivang257 <97958012+shivang257@users.noreply.github.com>
2023-03-21 01:03:08 +0530

    Time: 28 ms (30.81%), Space: 13.4 MB (79.07%) - LeetHub

---
+++ 0060-permutation-sequence/0060-permutation-sequence.py
@@ -0,0 +1,18 @@
+import math
+class Solution:
+    # @param {integer} n
+    # @param {integer} k
+    # @return {string}
+    def getPermutation(self, n, k):
+        numbers = range(1, n+1)
+        permutation = ''
+        k -= 1
+        while n > 0:
+            n -= 1
+            # get the index of current digit
+            index, k = divmod(k, math.factorial(n))
+            permutation += str(numbers[index])
+            # remove handled number
+            numbers.remove(numbers[index])
+
+        return permutation
