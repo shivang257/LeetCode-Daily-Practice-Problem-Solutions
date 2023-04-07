d65acb4b09444ca6ae93e139161d0696e81add59
shivang257 <97958012+shivang257@users.noreply.github.com>
2023-04-07 12:10:54 +0530

    Time: 172 ms (89.87%), Space: 23.6 MB (99.15%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -3,9 +3,9 @@ class Solution:
         n=len(nums)
         lo,hi=0,len(nums)-1
         while lo<hi:
-            mid= 2*((lo+hi)//4)
-            if nums[mid]==nums[mid+1]:
-                lo=mid+2
+            mid=(lo+hi)//2
+            if mid%2==0 and nums[mid]==nums[mid+1] or mid%2!=0 and nums[mid]==nums[mid-1]:
+                lo=mid+1
             else:
                 hi=mid
         return nums[lo]
