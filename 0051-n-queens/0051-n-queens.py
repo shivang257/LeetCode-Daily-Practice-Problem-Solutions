04cac74c2b12d2ca488a04d1d8c8847f40324334
shivang257 <97958012+shivang257@users.noreply.github.com>
2023-04-15 23:55:49 +0530

    Time: 60 ms (56.15%), Space: 14.4 MB (78.15%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,13 +1,13 @@
 class Solution:
-    def solveNQueens(self, n):
-        def DFS(queens, xy_dif, xy_sum):
-            p = len(queens)
-            if p==n:
-                result.append(queens)
-                return None
-            for q in range(n):
-                if q not in queens and p-q not in xy_dif and p+q not in xy_sum: 
-                    DFS(queens+[q], xy_dif+[p-q], xy_sum+[p+q])  
-        result = []
-        DFS([],[],[])
-        return [ ["."*i + "Q" + "."*(n-i-1) for i in sol] for sol in result]
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        res=[]
+        def backtrack(queens,posDia,negDia):
+            p=len(queens)
+            if len(queens)==n:
+                res.append(queens)
+                return
+            for c in range(n):
+                if c not in queens and p-c not in negDia and p+c not in posDia:
+                    backtrack(queens+[c],posDia+[p+c],negDia+[p-c])
+        backtrack([],[],[])
+        return [['.'*i+'Q'+'.'*(n-i-1) for i in sol] for sol in res]
