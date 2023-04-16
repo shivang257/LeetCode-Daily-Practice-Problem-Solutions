df9088c12153a49f09bde8d958cc88e5832360ee
shivang257 <97958012+shivang257@users.noreply.github.com>
2023-04-16 15:11:21 +0530

    Time: 114 ms (65.66%), Space: 14 MB (46.57%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.py
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.py
@@ -1,20 +1,20 @@
-class Solution(object):
-    def canPartitionKSubsets(self, nums, k):
-        sums = [0]*k
-        subsum = sum(nums) // k
+class Solution:
+    def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
+        total=sum(nums)//k
         nums.sort(reverse=True)
-        l = len(nums)
-        
-        def walk(i):
-            if i == l:
-                return len(set(sums)) == 1
+        sums=[0]*k
+        n=len(nums)
+        if sum(nums)%k!=0:
+            return False
+        def backtrack(i):
+            if i==n:
+                return len(set(sums))==1
             for j in range(k):
-                sums[j] += nums[i]
-                if sums[j] <= subsum and walk(i+1):
+                sums[j]+=nums[i]
+                if sums[j]<=total and backtrack(i+1):
                     return True
-                sums[j] -= nums[i]
-                if sums[j] == 0:
+                sums[j]-=nums[i]
+                if sums[j]==0:
                     break
-            return False        
-        
-        return walk(0)
+            return False
+        return backtrack(0)
