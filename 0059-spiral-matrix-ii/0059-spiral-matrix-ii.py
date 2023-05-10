6e26befa26d17d6ac9052253430cf4355fb6690b
shivang257 <97958012+shivang257@users.noreply.github.com>
2023-05-11 00:23:41 +0530

    Time: 43 ms (16.32%), Space: 16.3 MB (5.62%) - LeetHub

---
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.py
@@ -0,0 +1,11 @@
+class Solution:
+    def generateMatrix(self, n: int) -> List[List[int]]:
+        A = [[0] * n for _ in range(n)]
+        i, j, di, dj = 0, 0, 0, 1
+        for k in range(n*n):
+            A[i][j] = k + 1
+            if A[(i+di)%n][(j+dj)%n]:
+                di, dj = dj, -di
+            i += di
+            j += dj
+        return A
