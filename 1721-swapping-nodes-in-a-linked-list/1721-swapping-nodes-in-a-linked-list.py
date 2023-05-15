ff7f132e84f439aafee5e2d910a965539b15c9ea
shivang257 <97958012+shivang257@users.noreply.github.com>
2023-05-15 15:12:10 +0530

    Time: 951 ms (91.09%), Space: 50.6 MB (28.37%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.py
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.py
@@ -1,10 +1,17 @@
 class Solution:
     def swapNodes(self, head: ListNode, k: int) -> ListNode:
         slow, fast = head, head
+        
+# move the fast pointer to kth node
         for _ in range(k - 1):
             fast = fast.next
         first = fast
+        
+# move the fast pointer to end of the linked list 
+# then slow pointer will be at kth node from the end
         while fast.next:
             slow, fast = slow.next, fast.next
+            
+# swap the values
         first.val, slow.val = slow.val, first.val
         return head
