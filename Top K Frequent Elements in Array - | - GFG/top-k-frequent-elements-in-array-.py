ce0d9392a81d2bac9fc58a3899cbb8ac0b2b0013
shivang257 <97958012+shivang257@users.noreply.github.com>
2023-05-19 18:39:06 +0530

    Added solution - LeetHub

---
+++ Top K Frequent Elements in Array - | - GFG/top-k-frequent-elements-in-array-.py
@@ -0,0 +1,30 @@
+import collections
+class Solution:
+	def topK(self, nums, k):
+		#Code here
+		nums.sort(reverse=True)
+		a=collections.Counter(nums).most_common()
+		a = sorted(a, key=lambda x: x[0],
+               reverse=True)
+        a = sorted(a, key=lambda x: x[1],reverse=True)
+		l=[]
+		for i in range((k)):
+		    l.append(a[i][0])
+		
+		return l
+#{ 
+ # Driver Code Starts
+		
+if __name__ == '__main__':
+    T=int(input())
+    for i in range(T):
+    	a = list(map(int, input().strip().split()))
+    	n = a[0]
+    	nums = a[1:]
+    	k = int(input().strip())
+    	obj = Solution()
+    	ans = obj.topK(nums, k)
+    	for i in ans:
+    		print(i, end = " ")
+    	print()
+# } Driver Code Ends
