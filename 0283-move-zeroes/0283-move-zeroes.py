32a8cdfeaf52408e7428e4073194efee881884e6
shivang257 <97958012+shivang257@users.noreply.github.com>
2023-05-23 19:03:10 +0530

    Time: 183 ms (33.58%), Space: 18 MB (19.33%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,6 +1,6 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        left,right=0,1
+        left,right=0,0
         while right<len(nums):
             if nums[right]!=0 and nums[left]==0:
                 nums[left],nums[right]=nums[right],nums[left]
