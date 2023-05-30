631b78f229a1acaa010349edd101e25a6c42bc2c
shivang257 <97958012+shivang257@users.noreply.github.com>
2023-05-30 15:14:35 +0530

    Time: 278 ms (39.36%), Space: 25 MB (16.10%) - LeetHub

---
+++ 0705-design-hashset/0705-design-hashset.py
@@ -0,0 +1,20 @@
+class MyHashSet: 
+    def eval_hash(self, key):
+        return ((key*1031237) & (1<<20) - 1)>>5
+
+    def __init__(self):
+        self.arr = [[] for _ in range(1<<15)]
+
+    def add(self, key: int) -> None:
+        t = self.eval_hash(key)
+        if key not in self.arr[t]:
+            self.arr[t].append(key)
+
+    def remove(self, key: int) -> None:
+        t = self.eval_hash(key)
+        if key in self.arr[t]:
+            self.arr[t].remove(key)
+
+    def contains(self, key: int) -> bool:
+        t = self.eval_hash(key)
+        return key in self.arr[t]
