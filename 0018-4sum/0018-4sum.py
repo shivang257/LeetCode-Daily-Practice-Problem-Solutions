64b727c68b8dff93719ed032b362f9a8cffed0fe
shivang257 <97958012+shivang257@users.noreply.github.com>
2023-06-02 21:06:37 +0530

    Time: 1219 ms (37.14%), Space: 16.4 MB (23.28%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -1,14 +1,20 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         n = len(nums)
-        seen = set()
-        ans = set()
+        nums.sort()
+        ans=set()
         for i in range(n):
-            for j in range(i+1, n):
-                for k in range(j+1, n):
-                    lastNumber = target - nums[i] - nums[j] - nums[k]
-                    if lastNumber in seen:
-                        arr = sorted([nums[i], nums[j], nums[k], lastNumber])
-                        ans.add((arr[0], arr[1], arr[2], arr[3]))
-            seen.add(nums[i])
-        return ans
+            for j in range(i+1,n):
+                l,r=j+1,n-1
+                remain=target-nums[i]-nums[j]
+                
+                while l<r:
+                    if nums[l]+nums[r]==remain:
+                        ans.add((nums[i],nums[j],nums[l],nums[r]))
+                        l+=1
+                        r-=1
+                    elif nums[l]+nums[r]>remain:
+                        r-=1
+                    else:
+                        l+=1
+        return (ans)
