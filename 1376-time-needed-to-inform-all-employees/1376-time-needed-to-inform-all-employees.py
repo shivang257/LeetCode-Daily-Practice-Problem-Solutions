aeaa87fa5aa4c1b81c4d94904dd8076228a8d430
shivang257 <97958012+shivang257@users.noreply.github.com>
2023-06-03 09:57:19 +0530

    Time: 1136 ms (94.93%), Space: 42.6 MB (74.67%) - LeetHub

---
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.py
@@ -0,0 +1,8 @@
+class Solution:
+    def numOfMinutes(self, n, headID, manager, informTime):
+        def dfs(i):
+            if manager[i] != -1:
+                informTime[i] += dfs(manager[i])
+                manager[i] = -1
+            return informTime[i]
+        return max(map(dfs, range(n)))
