b3b77989baf07b5c7df8d851e9529bd2b29df702
shivang257 <97958012+shivang257@users.noreply.github.com>
2023-06-13 02:54:48 +0530

    Time: 3156 ms (30.04%), Space: 24.4 MB (7.65%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,22 +1,38 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        l,r=0,0
+        longest_palindrom = ''
+        dp = [[0]*len(s) for _ in range(len(s))]
+        #filling out the diagonal by 1
+        for i in range(len(s)):
+            dp[i][i] = True
+            longest_palindrom = s[i]
+			
+        # filling the dp table
+        for i in range(len(s)-1,-1,-1):
+				# j starts from the i location : to only work on the upper side of the diagonal 
+            for j in range(i+1,len(s)):  
+                if s[i] == s[j]:  #if the chars mathces
+                    # if len slicied sub_string is just one letter if the characters are equal, we can say they are palindomr dp[i][j] =True 
+                    #if the slicied sub_string is longer than 1, then we should check if the inner string is also palindrom (check dp[i+1][j-1] is True)
+                    if j-i ==1 or dp[i+1][j-1] is True:
+                        dp[i][j] = True
+                        # we also need to keep track of the maximum palindrom sequence 
+                        if len(longest_palindrom) < len(s[i:j+1]):
+                            longest_palindrom = s[i:j+1]
+                
+        return longest_palindrom
         n=len(s)
-        res=""
+        l=''
+        dp=[[0]*(len(s))]*(len(s))
         for i in range(n):
-            l,r=i,i
-            while l>=0 and r<len(s) and s[l]==s[r]:
-                l-=1
-                r+=1
-            tem=s[l+1:r]
-            if len(tem)>len(res):
-                res=tem
-        for i in range(n):
-            l,r=i,i+1
-            while l>=0 and r<len(s) and s[l]==s[r]:
-                l-=1
-                r+=1
-            tem=s[l+1:r]
-            if len(tem)>len(res):
-                res=tem
-        return res
+            dp[i][i]=True
+            l=s[i]
+        for i in range(n-1,-1,-1):
+            for j in range(i+1,n):
+                if s[i]==s[j]:
+                    if j-i==1 or dp[i+1][j-1] is True:
+                        dp[i][j]=True
+                        
+                        if len(l)<len(s[i:j+1]):
+                            l=s[i:j+1]
+        return l
