513b62bcbf16cb1320bae6782448b5e1f0d1cd5c
shivang257 <97958012+shivang257@users.noreply.github.com>
2023-06-15 02:15:41 +0530

    Added solution - LeetHub

--- Number of occurrence - GFG/number-of-occurrence.py
+++ Number of occurrence - GFG/number-of-occurrence.py
@@ -1,11 +1,26 @@
 #User function Template for python3
 from collections import Counter
 class Solution:
-
+    def binary(self,nums,target,leftbias):
+        left,right=0,len(nums)-1
+        i=-1
+        while left<=right:
+            mid=(left+right)//2
+            if nums[mid]<target:
+                left=mid+1
+            elif nums[mid]>target:
+                right=mid-1
+            else:
+                i=mid
+                if leftbias:
+                    right=mid-1
+                else:
+                    left=mid+1
+        return i
 	def count(self,arr, n, x):
-		# code here
-		a=Counter(arr)
-		return a[x] if a[x] else 0
+		left=self.binary(arr,x,True)
+		right=self.binary(arr,x,False)
+		return right-left+1 if x in arr else 0
 
 
 #{ 
