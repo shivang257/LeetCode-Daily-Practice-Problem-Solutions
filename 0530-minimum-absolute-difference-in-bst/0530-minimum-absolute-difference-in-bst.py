0f8ebb87e126e5f0a97311902b7ccc6705d743c1
shivang257 <97958012+shivang257@users.noreply.github.com>
2023-06-15 01:29:36 +0530

    Time: 72 ms (45.83%), Space: 18.6 MB (70.71%) - LeetHub

---
+++ 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def getMinimumDifference(self, root):
+        def fn(node, lo, hi):
+            if not node: return hi - lo
+            left = fn(node.left, lo, node.val)
+            right = fn(node.right, node.val, hi)
+            return min(left, right)
+        return fn(root, float('-inf'), float('inf'))
