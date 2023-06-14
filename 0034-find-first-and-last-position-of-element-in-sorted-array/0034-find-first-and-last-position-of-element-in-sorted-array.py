b9a0ea879a4edfbb2bc8832903989fd37cd94067
shivang257 <97958012+shivang257@users.noreply.github.com>
2023-06-15 02:07:40 +0530

    Time: 109 ms (7.28%), Space: 17.6 MB (79.45%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,21 +1,21 @@
 class Solution:
-    def searchRange(self, nums: List[int], target: int) -> List[int]:
-        def binary(nums,target,leftbias):
-            left,right=0,len(nums)-1
-            i=-1
-            while left<=right:
-                mid=(left+right)//2
-                if nums[mid]<target:
-                    left=mid+1
-                elif nums[mid]>target:
+    def binary(self,nums,target,leftbias):
+        left,right=0,len(nums)-1
+        i=-1
+        while left<=right:
+            mid=(left+right)//2
+            if nums[mid]<target:
+                left=mid+1
+            elif nums[mid]>target:
+                right=mid-1
+            else:
+                i=mid
+                if leftbias:
                     right=mid-1
                 else:
-                    i=mid
-                    if leftbias:
-                        right=mid-1
-                    else:
-                        left=mid+1
-            return i
-        left=binary(nums,target,True)
-        right=binary(nums,target,False)
+                    left=mid+1
+        return i
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        left=self.binary(nums,target,True)
+        right=self.binary(nums,target,False)
         return [left,right]
