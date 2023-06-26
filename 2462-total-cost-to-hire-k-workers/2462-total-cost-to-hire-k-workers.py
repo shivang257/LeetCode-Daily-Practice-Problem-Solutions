926f4b9413c0ab515cb8d4208cf454cc0fa030cd
shivang257 <97958012+shivang257@users.noreply.github.com>
2023-06-27 02:24:51 +0530

    Time: 787 ms (81.76%), Space: 26.9 MB (82.28%) - LeetHub

---
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.py
@@ -0,0 +1,20 @@
+class Solution:
+    def totalCost(self, costs: List[int], k: int, candidates: int) -> int:
+        q = costs[:candidates]
+        qq = costs[max(candidates, len(costs)-candidates):]
+        heapify(q)
+        heapify(qq)
+        ans = 0 
+        i, ii = candidates, len(costs)-candidates-1
+        for _ in range(k): 
+            if not qq or q and q[0] <= qq[0]: 
+                ans += heappop(q)
+                if i <= ii: 
+                    heappush(q, costs[i])
+                    i += 1
+            else: 
+                ans += heappop(qq)
+                if i <= ii: 
+                    heappush(qq, costs[ii])
+                    ii -= 1
+        return ans 
