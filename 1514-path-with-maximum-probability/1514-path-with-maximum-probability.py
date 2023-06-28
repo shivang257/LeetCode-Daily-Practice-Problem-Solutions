06f1144a3c966af2c012bce15ca6c32ac1f45867
shivang257 <97958012+shivang257@users.noreply.github.com>
2023-06-28 10:00:27 +0530

    Time: 729 ms (49.66%), Space: 30.3 MB (5.84%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.py
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.py
@@ -3,17 +3,17 @@ from math import log2
 
 class Solution:
     def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start: int, end: int) -> float:
-        AdjList = [set() for _ in range(n)]
+        adj = [set() for _ in range(n)]
         for (u, v), p in zip(edges, succProb):
-            AdjList[u].add((v, log2(1/p)))
-            AdjList[v].add((u, log2(1/p)))
+            adj[u].add((v, log2(1/p)))
+            adj[v].add((u, log2(1/p)))
         dist = [float('inf') for _ in range(n)]
         dist[start] = 0
         h = [(0, start)]
         while h:
             d, u = heappop(h)
             if d == dist[u]:
-                for (v, p) in AdjList[u]:
+                for (v, p) in adj[u]:
                     if dist[u] + p < dist[v]:
                         dist[v] = dist[u] + p
                         heappush(h, (dist[v], v))
