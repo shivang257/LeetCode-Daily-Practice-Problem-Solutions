9b0c2d9c72c640e1b222ef51fa10ed15d9dde8fa
shivang257 <97958012+shivang257@users.noreply.github.com>
2023-07-18 23:49:05 +0530

    Time: 711 ms (87.19%), Space: 77.4 MB (86.83%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.py
@@ -0,0 +1,17 @@
+from collections import OrderedDict
+class LRUCache:
+    def __init__(self, Capacity):
+        self.size = Capacity
+        self.cache = OrderedDict()
+
+    def get(self, key):
+        if key not in self.cache: return -1
+        val = self.cache[key]
+        self.cache.move_to_end(key)
+        return val
+
+    def put(self, key, val):
+        if key in self.cache: del self.cache[key]
+        self.cache[key] = val
+        if len(self.cache) > self.size:
+            self.cache.popitem(last=False)
