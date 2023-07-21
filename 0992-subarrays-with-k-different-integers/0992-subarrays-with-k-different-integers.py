63705f35d6d76d434f97c9660241df7801fee61b
shivang257 <97958012+shivang257@users.noreply.github.com>
2023-07-21 14:38:25 +0530

    Time: 536 ms (43.75%), Space: 19.4 MB (57.29%) - LeetHub

---
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.py
@@ -0,0 +1,16 @@
+class Solution:
+    def atmost(self,nums,k):
+        l=0
+        mp=defaultdict(int)
+        res=0
+        for r in range(len(nums)):
+            mp[nums[r]]+=1
+            while len(mp)==k+1:
+                mp[nums[l]]-=1
+                if mp[nums[l]]==0:
+                    mp.pop(nums[l])
+                l+=1
+            res+=r-l+1
+        return res
+    def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
+        return self.atmost(nums,k)-self.atmost(nums,k-1)
