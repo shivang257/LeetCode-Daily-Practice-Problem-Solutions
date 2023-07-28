624b9f63331de686c6c6822ef35f0797da7900f1
shivang257 <97958012+shivang257@users.noreply.github.com>
2023-07-28 08:46:40 +0530

    Time: 41 ms (96.36%), Space: 16.6 MB (31.92%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -0,0 +1,11 @@
+class Solution:
+    def PredictTheWinner(self, nums: List[int]) -> bool:
+        dp={}
+        
+        def find(i,j):
+            if i==j:
+                return nums[i]
+            if (i,j) not in dp:
+                dp[i,j]=max(nums[i]-find(i+1,j), nums[j]-find(i,j-1))
+            return dp[i,j]
+        return find(0,len(nums)-1)>=0
