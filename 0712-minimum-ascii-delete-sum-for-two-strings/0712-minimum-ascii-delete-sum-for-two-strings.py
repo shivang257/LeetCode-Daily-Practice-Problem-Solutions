c6854d258d6a76e0df7aeeacf7fbd2b6ed52eb64
shivang257 <97958012+shivang257@users.noreply.github.com>
2023-07-31 13:51:57 +0530

    Time: 340 ms (98.64%), Space: 17.5 MB (79.09%) - LeetHub

---
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minimumDeleteSum(self, s1: str, s2: str) -> int:
+        l1, l2 = len(s1), len(s2)
+        dp = [[0] * (l2 + 1) for _ in range(l1 + 1)]
+        for i in range(l1):
+            for j in range(l2):
+                if s1[i] == s2[j]:
+                    dp[i + 1][j + 1] = dp[i][j] + ord(s1[i])
+                else:
+                    dp[i + 1][j + 1] = max(dp[i][j + 1], dp[i + 1][j])
+        result = sum(map(ord, s1 + s2)) - dp[l1][l2] * 2
+        return result
