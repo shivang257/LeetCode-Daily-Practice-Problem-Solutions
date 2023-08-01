15062ca98a6e3cd9f21db95239748de28d8dd9a1
shivang257 <97958012+shivang257@users.noreply.github.com>
2023-08-01 22:20:06 +0530

    Time: 80 ms (98.92%), Space: 18.4 MB (34.56%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,14 +1,5 @@
 class Solution:
-    def combine(self, n: int, k: int) -> List[List[int]]:
-        res=[]
-        
-        def back(start,comb):
-            if len(comb)==k:
-                res.append(comb[:])
-                return
-            for i in range(start,n+1):
-                comb.append(i)
-                back(i+1,comb)
-                comb.pop()
-        back(1,[])
-        return res
+    def combine(self, n, k):
+        if k == 0:
+            return [[]]
+        return [pre + [i] for i in range(k, n+1) for pre in self.combine(i-1, k-1)]
