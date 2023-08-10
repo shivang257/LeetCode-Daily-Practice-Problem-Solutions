45bfeefbc20fab1cf7ef4b77c78de2938201fe9b
shivang257 <97958012+shivang257@users.noreply.github.com>
2023-08-10 22:54:59 +0530

    Time: 120 ms (89.60%), Space: 17.5 MB (48.39%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,40 +1,11 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        
-        size = len(nums)
-        
-        # destination is last index
-        destination = size - 1
-        
-        # record of current coverage, record of last jump index
-        cur_coverage, last_jump_index = 0, 0
-        
-        # counter for jump
-        times_of_jump = 0
-        
-         # Quick response if start index == destination index == 0
-        if size == 1:
-            return 0
-        
-        
-        # Greedy strategy: extend coverage as long as possible with lazy jump
-        for i in range( 0, size):
-            
-            # extend current coverage as further as possible
-            cur_coverage = max( cur_coverage, i + nums[i] )
-            
+        # the initial range (after 0th jump) is [0,0]
+        l = r = 0
+        nJumps = 0
+        while r < len(nums) - 1:
+            nJumps += 1
+            furthest = max([i + nums[i] for i in range(l,r+1)])
+            l,r = r+1, furthest
 
-            # forced to jump (by lazy jump) to extend coverage  
-            if i == last_jump_index:
-            
-                # update last jump index
-                last_jump_index = cur_coverage
-                
-                # update counter of jump by +1
-                times_of_jump += 1
-                
-                # check if reached destination already
-                if cur_coverage >= destination:
-                        return times_of_jump
-                
-        return times_of_jump
+        return nJumps
