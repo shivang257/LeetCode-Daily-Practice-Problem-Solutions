4f2fec9454de7d12e251e2843a224d6a60f21670
shivang257 <97958012+shivang257@users.noreply.github.com>
2023-08-18 01:01:56 +0530

    Time: 421 ms (99.31%), Space: 18.9 MB (94.85%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.py
@@ -0,0 +1,19 @@
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        m, n = len(mat), len(mat[0])
+
+        for r in range(m):
+            for c in range(n):
+                if mat[r][c] > 0:
+                    top = mat[r - 1][c] if r > 0 else math.inf
+                    left = mat[r][c - 1] if c > 0 else math.inf
+                    mat[r][c] = min(top, left) + 1
+
+        for r in range(m - 1, -1, -1):
+            for c in range(n - 1, -1, -1):
+                if mat[r][c] > 0:
+                    bottom = mat[r + 1][c] if r < m - 1 else math.inf
+                    right = mat[r][c + 1] if c < n - 1 else math.inf
+                    mat[r][c] = min(mat[r][c], bottom + 1, right + 1)
+
+        return mat
