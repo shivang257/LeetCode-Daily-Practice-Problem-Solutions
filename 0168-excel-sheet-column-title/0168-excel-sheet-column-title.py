3467c235fe089ec0381b2edeca989c9ad110c887
shivang257 <97958012+shivang257@users.noreply.github.com>
2023-08-22 23:58:25 +0530

    Time: 32 ms (93.98%), Space: 16.1 MB (99.26%) - LeetHub

---
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.py
@@ -0,0 +1,9 @@
+class Solution:
+    def convertToTitle(self, num: int) -> str:
+        capitals = [chr(x) for x in range(ord('A'), ord('Z')+1)]
+        result = []
+        while num > 0:
+            result.append(capitals[(num-1)%26])
+            num = (num-1) // 26
+        result.reverse()
+        return ''.join(result)
