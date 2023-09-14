40f03a8e8607a048ca8910f6948ff6aa2be5c611
shivang257 <97958012+shivang257@users.noreply.github.com>
2023-09-14 16:31:20 +0530

    Time: 84 ms (80.14%), Space: 16.5 MB (99.00%) - LeetHub

---
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findItinerary(self, tickets):
+        targets = collections.defaultdict(list)
+        for a, b in sorted(tickets)[::-1]:
+            targets[a] += b,
+        route, stack = [], ['JFK']
+        while stack:
+            while targets[stack[-1]]:
+                stack += targets[stack[-1]].pop(),
+            route += stack.pop(),
+        return route[::-1]
