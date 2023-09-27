e7801cc50cafde125ef8bddbf6eeee4024a0bd1d
shivang257 <97958012+shivang257@users.noreply.github.com>
2023-09-27 10:06:07 +0530

    Time: 44 ms (14.65%), Space: 16.3 MB (65.15%) - LeetHub

---
+++ 0880-decoded-string-at-index/0880-decoded-string-at-index.py
@@ -0,0 +1,14 @@
+class Solution:
+    def decodeAtIndex(self, S, K):
+        N = 0
+        for i, c in enumerate(S):
+            N = N * int(c) if c.isdigit() else N + 1
+            if K <= N: break
+        for j in range(i, -1, -1):
+            c = S[j]
+            if c.isdigit():
+                N /= int(c)
+                K %= N
+            else:
+                if K == N or K == 0: return c
+                N -= 1
