ab0c73bbe4c28affb9a850a354448587743b3a82
shivang257 <97958012+shivang257@users.noreply.github.com>
2023-10-30 00:22:12 +0530

    Time: 24 ms (99.20%), Space: 16.2 MB (74.40%) - LeetHub

---
+++ 0458-poor-pigs/0458-poor-pigs.py
@@ -0,0 +1,6 @@
+class Solution:
+    def poorPigs(self, buckets: int, minutesToDie: int, minutesToTest: int) -> int:
+        pigs = 0
+        while (minutesToTest // minutesToDie + 1) ** pigs < buckets:
+            pigs += 1
+        return pigs
