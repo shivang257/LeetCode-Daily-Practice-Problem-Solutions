0f9ff9041731a8d890543d97e09c3bd8de6fa35e
shivang257 <97958012+shivang257@users.noreply.github.com>
2024-03-02 21:57:17 +0530

    Time: 158 ms (50.27%), Space: 19 MB (35.01%) - LeetHub

---
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def sortedSquares(self, A):
+        answer = [0] * len(A)
+        l, r = 0, len(A) - 1
+        while l <= r:
+            left, right = abs(A[l]), abs(A[r])
+            if left > right:
+                answer[r - l] = left * left
+                l += 1
+            else:
+                answer[r - l] = right * right
+                r -= 1
+        return answer
