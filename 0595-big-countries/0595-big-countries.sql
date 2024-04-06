016eb32cfe46f3ccb2f27fb0435420b8c89a0c84
shivang257 <97958012+shivang257@users.noreply.github.com>
2024-04-07 01:56:34 +0530

    Time: 1353 ms (5.04%), Space: 0B (100.00%) - LeetHub

--- 0595-big-countries/0595-big-countries.sql
+++ 0595-big-countries/0595-big-countries.sql
@@ -1,1 +1,4 @@
-select name,population, area from World where area>=3000000 or population>=25000000;
+select name,population, area from World where area>=3000000 
+union
+
+select name,population, area from World where population>=25000000;
