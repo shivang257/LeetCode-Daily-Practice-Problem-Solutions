4455c19ad3452ff603deb1973e84026107e60534
shivang257 <97958012+shivang257@users.noreply.github.com>
2024-04-14 15:25:38 +0530

    Time: 2562 ms (28.11%), Space: 0B (100.00%) - LeetHub

--- 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -1,1 +1,1 @@
-select p.product_name, s.year, s.price from Sales s join product p where s.product_id=p.product_id;
+select Product.product_name, Sales.year, Sales.price from Sales left join Product on Sales.product_id = Product.product_id
