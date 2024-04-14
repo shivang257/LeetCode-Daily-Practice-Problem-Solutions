d22be98fe6156aef9246a650916f0690016fd698
shivang257 <97958012+shivang257@users.noreply.github.com>
2024-04-14 14:53:51 +0530

    Time: 1987 ms (86.53%), Space: 0B (100.00%) - LeetHub

--- 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
+++ 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
@@ -1,3 +1,1 @@
-select 
-eu.unique_id as unique_id, e.name as name
-from Employees e left join EmployeeUNI eu on e.id = eu.id
+select eu.unique_id, e.name from Employees e left join EmployeeUNI eu on e.id = eu.id
