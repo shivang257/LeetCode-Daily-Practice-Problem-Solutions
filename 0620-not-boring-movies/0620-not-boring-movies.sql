43842e9282afbb6f18bea51910e8b6ac2be31df1
shivang257 <97958012+shivang257@users.noreply.github.com>
2024-04-22 00:41:59 +0530

    Time: 344 ms (99.94%), Space: 0B (100.00%) - LeetHub

--- 0620-not-boring-movies/0620-not-boring-movies.sql
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -1,1 +1,1 @@
-select id, movie, description , rating from Cinema where id%2=1 and description!='boring' order by rating desc;
+select id, movie, description , rating from Cinema where mod(id,2) = 1 and description!='boring' order by rating desc;
