e46119d6edd09e6f6ded244e8def73a741dfd033
shivang257 <97958012+shivang257@users.noreply.github.com>
2024-04-30 18:35:22 +0530

    Time: 1021 ms (71.19%), Space: 0B (100.00%) - LeetHub

--- 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
+++ 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
@@ -1,2 +1,3 @@
-select customer_id from
-(select customer_id from customer group by customer_id having count(distinct(product_key)) = (select count(product_key) from product)) as x
+select customer_id from customer 
+group by customer_id 
+having count(distinct(product_key)) = (select count(product_key) from product)
