b3f0d55e5d61345a908804307e24ef856a5013f8
shivang257 <97958012+shivang257@users.noreply.github.com>
2024-05-07 10:42:00 +0530

    Time: 211 ms (95.77%), Space: 19.3 MB (64.32%) - LeetHub

--- 2816-double-a-number-represented-as-a-linked-list/2816-double-a-number-represented-as-a-linked-list.py
+++ 2816-double-a-number-represented-as-a-linked-list/2816-double-a-number-represented-as-a-linked-list.py
@@ -1,8 +1,3 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
     def doubleIt(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if head.val > 4:
