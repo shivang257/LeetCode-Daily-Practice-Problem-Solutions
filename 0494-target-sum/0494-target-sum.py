218455681c0e4fd2a8a95dbcdf5e0b8fcf0b23cc
shivang257 <97958012+shivang257@users.noreply.github.com>
2024-12-26 22:59:50 +0530

    Time: 161 ms (61.31%), Space: 18.2 MB (59.59%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findTargetSumWays(self, A, S):
+        count = collections.Counter({0: 1})
+        for x in A:
+            step = collections.Counter()
+            for y in count:
+                step[y + x] += count[y]
+                step[y - x] += count[y]
+            count = step
+        return count[S]
